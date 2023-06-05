73dadd7d6b17fcbfb24a694d402d9b9cf91a5c59
Gabriel Torres <thomastgtorres21@gmail.com>
2023-06-05 10:17:17 -0500

    feat: add ride class and attributes

---
+++ lib/ride.rb
@@ -0,0 +1,17 @@
+class Ride
+
+    attr_reader :name,
+                :distance, 
+                :loop, 
+                :terrain
+  def initialize(info)
+    @name = info[:name]
+    @distance = info[:distance]
+    @loop = info[:loop]
+    @terrain =  info[:terrain]
+  end
+
+  def loop?
+    @loop
+  end
+end
--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -5,7 +5,24 @@ RSpec.describe Ride do
     it 'exist' do
       ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
 
-      expect(ride).to_be_an_instance of(Ride)
+      expect(ride1).to be_an_instance_of(Ride)
+    end
+
+    it 'is initialized with attributes' do
+      ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+
+      expect(ride1.name).to eq("Walnut Creek Trail")
+      expect(ride1.distance).to eq(10.7)
+      expect(ride1.loop).to eq(false)
+      expect(ride1.terrain).to eq(:hills)
+    end
+
+    it 'can indicate if the ride is a loop or not' do 
+      ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+
+      ride1.loop? 
+
+      expect(ride1.loop?).to eq(false)
     end
   end
 end
